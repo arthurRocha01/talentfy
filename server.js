8ff4bf91cf9bc4cca838db3d13bd9f45b8a8c47f
JOAO THALES PORTELA FIAES <023.961173@universidadesenaicimatec.edu.br>
2025-10-27 16:16:27 -0300

    adciona estrutura

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 5005;
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor está rodando na porta ${PORT}`)
+});
