4263752f7495f9bfe6ee9528c1e150f973bfebb4
Arthur Rocha <arthsx7@gmail.com>
2025-10-28 18:26:45 -0300

    atualiza todos os componentes - arthur

--- src/shared/components/Button.jsx
+++ src/shared/components/Button.jsx
@@ -1,20 +1,9 @@
 import styles from './Button.module.css';
 
 export const Button = ({ text, onClick }) => {
-<<<<<<< HEAD
-  return (
-    <div className="containerBtn">
-      <button className={styles.button} onClick={onClick}>
-        {text}
-      </button>
-    </div>
-  );
-};
-=======
     return (
         <button className={styles.button} onClick={onClick}>
             {text}
         </button>
     );  
 }
->>>>>>> 221f97558ab968aecd0722f73456aa28acfef4ed
