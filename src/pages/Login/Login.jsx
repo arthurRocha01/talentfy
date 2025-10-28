9ebffee21f8dbc2466e5965bb78d7806a34ffa79
Arthur Rocha <arthsx7@gmail.com>
2025-10-28 17:22:50 -0300

    adiciona página Login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+import { Input } from '../../shared/components/Input.jsx';
+
+export const Login = () => {
+    return (
+        <div className="container">
+            <Input placeholder="Input..." />
+        </div>
+    );
+}
