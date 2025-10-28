15d66e0ecbce98a84bfd90c43241c3cdaca75cd9
Arthur Rocha <arthsx7@gmail.com>
2025-10-28 13:59:48 -0300

    melhora estrutura

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import { Input } from "../../shared/components/Input";
+
+export const Home = () => {
+    return (
+        <>
+            <Input text="Input" placeholder="input..." type="text" />
+        </>
+    );
+}
