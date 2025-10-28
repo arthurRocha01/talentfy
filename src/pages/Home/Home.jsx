4c23048aec61cb01060f264cb818bda8160c6e06
Arthur Rocha <arthsx7@gmail.com>
2025-10-28 16:00:20 -0300

    adciona Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
-import { Input } from "../../shared/components/Input";
+import styles from './Home.module.css';
+
 import { Navbar } from "../../shared/components/Navbar";
+import { Main } from "./components/Main";
 
 export const Home = () => {
     return (
-        <>
+        <div className={styles.container}>
             <Navbar />
-            {/* <Input text="Input" placeholder="input..." type="text" /> */}
-        </>
+            <Main />
+        </div>
     );
 }
