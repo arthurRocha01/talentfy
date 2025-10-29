97af4efebab3cc31e62cd467ec1f9e9c251e3197
Guilherme Gonçalves Batista <gguigoncalves@outlook.com>
2025-10-29 14:56:55 -0300

    Atualiza css dos componentes 2 (Gonçalves)

--- src/pages/Home/components/Main.jsx
+++ src/pages/Home/components/Main.jsx
@@ -3,8 +3,9 @@ import styles from './Main.module.css';
 export const Main = () => {
     return (
         <main className={styles.main}>
-            <p>Uma plataforma para ligar o técnico ao cliente em poucos cliques</p>
-            <p>Transformando seu serviço em conquistas. Nosso talento, sua vantagem competitiva. Conectados pelo profissionalismo.</p>
+            <p>Uma plataforma para ligar o técnico ao cliente em poucos cliques.</p>
+            <p>Transformando seu serviço em conquistas. <br />
+            Nosso talento, sua vantagem competitiva.</p>
         </main>
     );
 }
