bc663c213c0b6f2675ab0cac99d6ba89a778a712
ARTHUR DA ROCHA SANTOS <023.916783@universidadesenaicimatec.edu.br>
2025-11-03 13:49:41 -0300

    Revert "inicia feature de auth login/register - arthur"
    This reverts commit 213cdf298ef9d32bb59f37dab12669da097675d5.

--- server/src/routes/authRooutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as authControllers from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/login', authControllers.login);
-
-export default router;
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import * as userController from '../controllers/userController.js';
-import { authMiddleware } from '../middlewares/auth.js';
 
-const router = express.Router();
+export const router = express.Router();
 
-router.get('/', authMiddleware, userController.getAllUsers);
-router.get('/:id', authMiddleware, userController.getUserById);
-router.post('/', authMiddleware, userController.createUser);
-router.put('/:id', authMiddleware, userController.updateUser);
-router.delete('/:id', authMiddleware, userController.deleteUser);
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 export default router;
